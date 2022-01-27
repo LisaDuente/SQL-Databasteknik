5f596d895fa6d3975d29f98897b495a36e8d0d7c
LisaDuente <lisa.duente@gmail.com>
2022-01-27 10:53:36 +0100

    made salary tabel, changed adresses in Personal, Departments, Customers, Shippers

--- InlämningLisaDünte.sql
+++ InlämningLisaDünte.sql
@@ -18,20 +18,6 @@ Price FLOAT,
 PRIMARY KEY(ProductID)
 );
 
--- CREATE TABLE Warehouse ?
-
--- maybe not needed? Interesting for shippers?
-CREATE TABLE Departments(
-DepartmentID INT UNIQUE AUTO_INCREMENT,
-DepartmentName VARCHAR (255),
-DepartmentHead VARCHAR (255),
-DepartmentAdress VARCHAR(255),
-DepartmentPostCode INT,
-DepartmentCity VARCHAR (255),
-DepartmentCountry VARCHAR (255),
-PRIMARY KEY (DepartmentID)
-);
-
 -- in exchange for all the info in shippers, customers and personal? maybe no good because a shipper can have the same ID as a customer because they are on different tables?
 CREATE TABLE Adresses(
 ID INT UNIQUE,
@@ -41,53 +27,73 @@ PostCode INT,
 City VARCHAR (255),
 Country VARCHAR (255),
 PRIMARY KEY (ID)
+) DEFAULT CHARSET = Latin1;
+
+-- CREATE TABLE Warehouse ?
+
+-- maybe not needed? Interesting for shippers?
+CREATE TABLE Departments(
+DepartmentID INT UNIQUE AUTO_INCREMENT,
+DepartmentName VARCHAR (255),
+DepartmentHead VARCHAR (255),
+AdressID INT UNIQUE,
+FOREIGN KEY (AdressID) REFERENCES Adresses(ID),
+PRIMARY KEY (DepartmentID)
+);
+
+CREATE TABLE Salary(
+ID INT UNIQUE AUTO_INCREMENT,
+ContractBegin DATETIME DEFAULT NOW(),
+SalaryCategory VARCHAR (255),
+ContractForm VARCHAR (255),
+Salary FLOAT,
+PRIMARY KEY (ID)
 );
 
 CREATE TABLE Personal(
 PersonalID INT UNIQUE NOT NULL AUTO_INCREMENT,
 PersonalName VARCHAR (255),
-PersonalAdress VARCHAR(255),
-PersonalPostCode INT,
-PersonalCity VARCHAR (255),
-PersonalCountry VARCHAR (255),
-Position VARCHAR(255),
+AdressID INT UNIQUE,
 DepartmentID INT,
+Phone VARCHAR(20),
+Email VARCHAR (60),
+Position VARCHAR(255),
+SalaryID INT UNIQUE,
+FOREIGN KEY (SalaryID) REFERENCES Salary(ID),
 FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID),
+FOREIGN KEY (AdressID) REFERENCES Adresses(ID),
 PRIMARY KEY (PersonalID)
 );
 
 CREATE TABLE Shippers(
 ShipperID INT UNIQUE NOT NULL AUTO_INCREMENT,
 ShipperName VARCHAR (255),
-ShipperAdress VARCHAR (255),
-ShipperPostCode INT,
-ShipperCity VARCHAR (255),
-ShipperCountry VARCHAR (255),
+AdressID INT UNIQUE,
 ShipperPhone VARCHAR(255),
+FOREIGN KEY (AdressID) REFERENCES Adresses(ID),
 PRIMARY KEY (ShipperID)
 );
 
 CREATE TABLE Customers(
 CustomerID INT UNIQUE NOT NULL AUTO_INCREMENT,
 CustomerName VARCHAR (255),
-CustomerStreet VARCHAR (255),
-CustomerPostCode Int,
-CustomerCity VARCHAR (255),
-CustomerCountry VARCHAR (255),
+AdressID INT UNIQUE,
 CustomerPhone VARCHAR (255),
+CustomerEmail VARCHAR (255),
+FOREIGN KEY (AdressID) REFERENCES Adresses(ID),
 PRIMARY KEY (CustomerID)
-);
+) DEFAULT CHARSET = Latin1;
 
 -- Transaktionshistorik
 CREATE TABLE Orders(
 OrderID INT UNIQUE NOT NULL AUTO_INCREMENT,
-Customer INT,
+CustomerID INT,
 OrderDate DATETIME DEFAULT NOW(),
 ShippingDate DATETIME,
-Shipper INT,
+ShipperID INT,
 OrderStatus VARCHAR (255),
-FOREIGN KEY (Shipper) REFERENCES Shippers(ShipperID),
-FOREIGN KEY (Customer) REFERENCES Customers(CustomerID),
+FOREIGN KEY (ShipperID) REFERENCES Shippers(ShipperID),
+FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
 PRIMARY KEY (OrderID)
 );
 
@@ -99,7 +105,6 @@ PriceEach FLOAT,
 Quantity INT,
 FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
 FOREIGN KEY (ProductID) REFERENCES Products(ProductID),
--- FOREIGN KEY (PriceEach) REFERENCES Products(Price), doesn't work because its not part of the key, is it needed? how to make a view?
 PRIMARY KEY (OrderID, ProductID)
 );
 
