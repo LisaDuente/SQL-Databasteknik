feb20d57ca98ad8f3967f4dbb72bdceaafe183db
LisaDuente <lisa.duente@gmail.com>
2022-02-13 11:19:18 +0100

    Check quantity

--- IKEA DATABASE/CheckQuantity2.sql
+++ IKEA DATABASE/CheckQuantity2.sql
@@ -1,7 +1,8 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `checkQuantity2`(PID INT, Quantity INT)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `checkQuantity2`(PID INT, Quantity INT, INOUT checker INT)
 BEGIN
-SELECT @inStock := SUM(Quantity) FROM WarehouseStatus WHERE ProductID = PID;
+SELECT @inStock := SUM(WarehouseStatus.Quantity) FROM WarehouseStatus WHERE ProductID = PID;
 IF Quantity > @inStock THEN
 	Rollback;
+    SET checker = 0;
 END IF;
 END
